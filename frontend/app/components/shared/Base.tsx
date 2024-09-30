982052bb2d08b3e4a58cd04d83a53a210c0d960b
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-09-30 18:59:43 -0300

    Modificação: Alterando a base para alterar o tamanho do background

--- frontend/app/components/shared/Base.tsx
+++ frontend/app/components/shared/Base.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Background from "./Background";
@@ -8,11 +10,27 @@ type BaseProps = {
 };
 
 export default function Base({ children }: BaseProps) {
+  const backgroundRef = useRef<HTMLDivElement | null>(null);
+  const mainRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(()=>{
+    const background = backgroundRef.current;
+    const main = mainRef.current;
+
+    if(background && main){
+      const mainHeight = main.offsetHeight;
+      background.style.height = `${mainHeight}px`;
+    }
+  },[backgroundRef, mainRef]);
+
   return (
     <>
-      <Background />
+      <Background ref={backgroundRef} />
       <Navbar />
-      <main className="min-h-[150vh] container absolute pt-28 left-2/4 -translate-x-2/4">
+      <main
+        className="min-h-[150vh] container absolute pt-28 left-2/4 -translate-x-2/4"
+        ref={mainRef}
+      >
         {children}
         <Footer />
       </main>
