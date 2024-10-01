8e27c3bec76fc22aca5ef85b1341732b62615b55
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-10-01 19:46:17 -0300

    Criação: modelo da pagina que exibe os veiculos

---
+++ frontend/app/pages/veiculos/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@components/pages/veiculos/Header";
+import GridCars from "@root/app/components/pages/veiculos/GridCars";
+
+export default function Veiculos()
+{
+    return(
+        <>
+            <Header />
+            <GridCars />
+        </>
+    )
+}
