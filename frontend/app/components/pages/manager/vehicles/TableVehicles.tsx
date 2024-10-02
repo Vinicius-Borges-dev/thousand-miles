1633247522d5cdaa5e00d75542dc172607e91cf6
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-10-02 20:59:05 -0300

    Atualizar o TableVehicles.tsx

--- frontend/app/components/pages/manager/vehicles/TableVehicles.tsx
+++ frontend/app/components/pages/manager/vehicles/TableVehicles.tsx
@@ -20,102 +20,30 @@ export default async function TableVehicles() {
         </tr>
       </thead>
       <tbody className="*:text-black rounded-lg [&>tr>td]:py-3 [&>tr>td]:font-semibold [&>tr>td]:text-tbody-text-color [&>tr>td]:pl-2 [&>tr]:border-[2px] [&>tr]:border-b-table-border">
-        <tr>
-          <td>
-            <span className="flex gap-3">
-              <Image src={bmwCinza} height={30} alt="Veiculo" />
-              <span className="pl-2">I8</span>
-            </span>
-          </td>
-          <td>BMW</td>
-          <td>Carro</td>
-          <td>100,00</td>
-          <td>54</td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconEdit} alt="Icone de edição" />
-              Editar
-            </span>
-          </td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconDelete} alt="Icone de edição" />
-              Excluir
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <span className="flex gap-3">
-              <Image src={bmwCinza} height={30} alt="Veiculo" />
-              <span className="pl-2">I8</span>
-            </span>
-          </td>
-          <td>BMW</td>
-          <td>Carro</td>
-          <td>100,00</td>
-          <td>54</td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconEdit} alt="Icone de edição" />
-              Editar
-            </span>
-          </td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconDelete} alt="Icone de edição" />
-              Excluir
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <span className="flex gap-3">
-              <Image src={bmwCinza} height={30} alt="Veiculo" />
-              <span className="pl-2">I8</span>
-            </span>
-          </td>
-          <td>BMW</td>
-          <td>Carro</td>
-          <td>100,00</td>
-          <td>54</td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconEdit} alt="Icone de edição" />
-              Editar
-            </span>
-          </td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconDelete} alt="Icone de edição" />
-              Excluir
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <span className="flex gap-3">
-              <Image src={bmwCinza} height={30} alt="Veiculo" />
-              <span className="pl-2">I8</span>
-            </span>
-          </td>
-          <td>BMW</td>
-          <td>Carro</td>
-          <td>100,00</td>
-          <td>54</td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconEdit} alt="Icone de edição" />
-              Editar
-            </span>
-          </td>
-          <td>
-            <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-              <Image src={iconDelete} alt="Icone de edição" />
-              Excluir
-            </span>
-          </td>
-        </tr>
+        <tr className="first:[&>td:first-child]:rounded-tl-lg first:[&>td:last-child]:rounded-tr-lg last:[&>td:first-child]:rounded-bl-lg last:[&>td:last-child]:rounded-br-lg">
+    <td>
+      <span className="flex gap-3">
+        <Image src={bmwCinza} height={30} alt="Veiculo" />
+        <span className="pl-2">I8</span>
+      </span>
+    </td>
+    <td>BMW</td>
+    <td>Carro</td>
+    <td>100,00</td>
+    <td>54</td>
+    <td>
+      <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
+        <Image src={iconEdit} alt="Icone de edição" />
+        Editar
+      </span>
+    </td>
+    <td>
+      <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
+        <Image src={iconDelete} alt="Icone de edição" />
+        Excluir
+      </span>
+    </td>
+  </tr>
       </tbody>
     </Table>
   );
