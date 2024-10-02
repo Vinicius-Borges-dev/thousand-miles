cb381036a738e9abeabba004a6c6b5b533b9f6b1
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-10-02 20:03:02 -0300

    Criação: Pagina que exibe a tabela com os veiculos do sistema

---
+++ frontend/app/pages/manager/vehicles/page.tsx
@@ -0,0 +1,10 @@
+import TableVehicles from "@root/app/components/pages/manager/vehicles/TableVehicles";
+
+export default function ManagerVehicles()
+{
+    return(
+        <>
+            <TableVehicles />
+        </>
+    )
+}
