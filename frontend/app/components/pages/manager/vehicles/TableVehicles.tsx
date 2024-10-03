82eaccad1a21603642acbc839082ed0995ee6cfd
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-10-03 16:42:55 -0300

    Modificação: Alterando o modelo para preparo

--- frontend/app/components/pages/manager/vehicles/TableVehicles.tsx
+++ frontend/app/components/pages/manager/vehicles/TableVehicles.tsx
@@ -5,11 +5,12 @@ import Image from "next/image";
 import bmwCinza from "@images/bmw-cinza.png";
 import iconEdit from "@icons/iconEdit.svg";
 import iconDelete from "@icons/iconTrash.svg";
+import Link from "next/link";
 
 export default async function TableVehicles() {
   return (
     <Table>
-      <thead className="[&>tr>th]:text-left [&>tr>th]:pl-2">
+      <thead className="[&>tr>th]:text-left [&>tr>th]:pl-2 [&>tr>th]:pb-4 *:uppercase *:font-bold">
         <tr>
           <th>Modelo</th>
           <th>Marca</th>
@@ -19,31 +20,41 @@ export default async function TableVehicles() {
           <th colSpan={2}>Ações</th>
         </tr>
       </thead>
-      <tbody className="*:text-black rounded-lg [&>tr>td]:py-3 [&>tr>td]:font-semibold [&>tr>td]:text-tbody-text-color [&>tr>td]:pl-2 [&>tr]:border-[2px] [&>tr]:border-b-table-border">
-        <tr className="first:[&>td:first-child]:rounded-tl-lg first:[&>td:last-child]:rounded-tr-lg last:[&>td:first-child]:rounded-bl-lg last:[&>td:last-child]:rounded-br-lg">
-    <td>
-      <span className="flex gap-3">
-        <Image src={bmwCinza} height={30} alt="Veiculo" />
-        <span className="pl-2">I8</span>
-      </span>
-    </td>
-    <td>BMW</td>
-    <td>Carro</td>
-    <td>100,00</td>
-    <td>54</td>
-    <td>
-      <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-        <Image src={iconEdit} alt="Icone de edição" />
-        Editar
-      </span>
-    </td>
-    <td>
-      <span className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit">
-        <Image src={iconDelete} alt="Icone de edição" />
-        Excluir
-      </span>
-    </td>
-  </tr>
+      <tbody className="*:text-black rounded-lg [&>tr>td]:py-3 [&>tr>td]:font-semibold [&>tr>td]:text-tbody-text-color [&>tr>td]:pl-2">
+        {Array.from({ length: 5 }).map((_, index) => {
+          return (
+            <tr className="odd:bg-white even:bg-gray-300" key={index}>
+              <td>
+                <span className="flex gap-3">
+                  <Image src={bmwCinza} height={30} alt="Veiculo" />
+                  <span className="pl-2">I8</span>
+                </span>
+              </td>
+              <td>BMW</td>
+              <td>Carro</td>
+              <td>100,00</td>
+              <td>54</td>
+              <td>
+                <Link
+                  href={`/`}
+                  className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit"
+                >
+                  <Image src={iconEdit} alt="Icone de edição" />
+                  Editar
+                </Link>
+              </td>
+              <td>
+                <Link
+                  href={`/`}
+                  className="flex gap-3 border border-gray-600 rounded-md p-1 w-fit"
+                >
+                  <Image src={iconDelete} alt="Icone de edição" />
+                  Excluir
+                </Link>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </Table>
   );
