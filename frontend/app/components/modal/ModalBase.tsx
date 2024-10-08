66147039e74f216125fa893253c736848972624d
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-10-08 20:41:19 -0300

    Atualizar o ModalBase.tsx

--- frontend/app/components/modal/ModalBase.tsx
+++ frontend/app/components/modal/ModalBase.tsx
@@ -1,1 +1,10 @@
-.
+export default function ModalBase()
+{
+    return(
+        <section className="absolute top-0 left-0 w-screen h-screen bg-gray-700 bg-opacity-1">
+            <div className="">
+                <h1></h1>
+            </div>
+        </section>
+    )
+}
