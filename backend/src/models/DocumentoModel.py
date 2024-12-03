c521cab3fd1bd1143cf9155ef7f753d6ec2e2c48
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-12-03 19:32:26 -0300

    Criação: Modelo de documento.

---
+++ backend/src/models/DocumentoModel.py
@@ -0,0 +1,27 @@
+from src.configs.banco_de_dados import Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+
+class DocumentoModel(Base):
+    
+    __tablename__ = "documento"
+    
+    id_documento = Column(Integer, primary_key=True, autoincrement=True)
+    tipo_documento = Column(String(50), nullable=False)
+    numero_documento = Column(String(50), nullable=False)
+    
+    dados_pessoais = relationship('DadosPessoaisModel', back_populates='documento')
+    
+    def __init__(self, tipo_documento:str, numero_documento:str)->None:
+        self.tipo_documento = tipo_documento
+        self.numero_documento = numero_documento
+    
+    def __repr__(self)->str:
+        return f'Documento (id: {self.id_documento}, tipo_documento: {self.tipo_documento}, numero_documento: {self.numero_documento})'
+    
+    def to_dict(self)->dict:
+        return {
+            'id_documento': self.id_documento,
+            'tipo_documento': self.tipo_documento,
+            'numero_documento': self.numero_documento
+        }
