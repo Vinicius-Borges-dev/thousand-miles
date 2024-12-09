553380dd685e9ce93ebce9ce9e45ee293cea8d84
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-12-09 17:16:02 -0300

    Criação: Rotas para o combustível.

---
+++ backend/src/routes/CombustivelRoute.py
@@ -0,0 +1,39 @@
+from flask import Blueprint
+from src.controllers.CombustivelController import CombustivelController
+
+combustivel_bp = Blueprint("combustiveis", __name__)
+
+
+@combustivel_bp.route("/", methods=["POST"])
+def criar_combustivel():
+    return CombustivelController().criar_combustivel()
+
+
+@combustivel_bp.route("/", methods=["GET"])
+def buscar_todos_combustiveis():
+    return CombustivelController().buscar_todos_combustiveis()
+
+
+@combustivel_bp.route("/<int:id_combustivel>", methods=["GET"])
+def buscar_combustivel_por_id(id_combustivel: int):
+    return CombustivelController().buscar_combustivel_por_id(id_combustivel)
+
+
+@combustivel_bp.route("/nome/<string:nome_combustivel>", methods=["GET"])
+def buscar_combustivel_por_nome(nome_combustivel: str):
+    return CombustivelController().buscar_combustivel_por_nome(nome_combustivel)
+
+
+@combustivel_bp.route("/<int:id_combustivel>", methods=["PUT"])
+def atualizar_combustivel(id_combustivel: int):
+    return CombustivelController().atualizar_combustivel(id_combustivel)
+
+
+@combustivel_bp.route("/<int:id_combustivel>", methods=["DELETE"])
+def deletar_combustivel(id_combustivel: int):
+    return CombustivelController().deletar_combustivel(id_combustivel)
+
+
+@combustivel_bp.route("/semelhante/<string:nome_combustivel>", methods=["GET"])
+def buscar_por_nome_semelhante(nome_combustivel: str):
+    return CombustivelController().buscar_por_nome_semelhante(nome_combustivel)
