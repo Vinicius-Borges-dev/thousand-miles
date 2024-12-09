ee5676ed271c5dd309d67fd9c9685086c40ffd54
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-12-09 19:44:00 -0300

    Criação: Importação das rotas para uso na aplicação.

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -12,5 +12,25 @@ def create_app():
     from src.routes.UsuarioRoute import usuario_bp
     app.register_blueprint(usuario_bp, url_prefix="/usuario")
     
+    from src.routes.AlbumVeiculoRoute import album_veiculo_bp
+    app.register_blueprint(album_veiculo_bp, url_prefix="/album_veiculo")
+    
+    from src.routes.CambioRoute import cambio_bp
+    app.register_blueprint(cambio_bp, url_prefix="/cambio")
+    
+    from src.routes.CategoriaRoute import categoria_bp
+    app.register_blueprint(categoria_bp, url_prefix="/categoria")
+    
+    from src.routes.CombustivelRoute import combustivel_bp
+    app.register_blueprint(combustivel_bp, url_prefix="/combustivel")
+    
+    from src.routes.ModeloRoute import modelo_bp
+    app.register_blueprint(modelo_bp, url_prefix="/modelo")
+    
+    from src.routes.VeiculoRoute import veiculo_bp
+    app.register_blueprint(veiculo_bp, url_prefix="/veiculo")
+    
+    from src.routes.MarcaRoute import marca_bp
+    app.register_blueprint(marca_bp, url_prefix="/marca")
 
     return app
