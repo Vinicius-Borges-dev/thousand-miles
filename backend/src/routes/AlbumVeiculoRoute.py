f69e06a477701eb837825e02ec059c9798eb62cb
Vinícius Borges <dev.vinicius.borges@outlook.com>
2024-12-09 19:40:54 -0300

    Criação: Rota para busca por ID.

--- backend/src/routes/AlbumVeiculoRoute.py
+++ backend/src/routes/AlbumVeiculoRoute.py
@@ -14,16 +14,11 @@ def buscar_album_por_id(id_album: int):
     return AlbumVeiculoController().buscar_album_veiculo_por_id(id_album)
 
 
-@album_veiculo_bp.route("/<int:id_album>", methods=["GET"])
-def buscar_album_por_id(id_album: int):
-    return AlbumVeiculoController().buscar_album_veiculo_por_id(id_album)
-
-
 @album_veiculo_bp.route("/<int:id_album>", methods=["PUT"])
-def atualizar_album_por_id(id_album: int):
+def atualizar_album(id_album: int):
     return AlbumVeiculoController().atualizar_album_veiculo(id_album)
 
 
 @album_veiculo_bp.route("/<int:id_album>", methods=["DELETE"])
-def deletar_album_por_id(id_album: int):
+def deletar_album(id_album: int):
     return AlbumVeiculoController().deletar_album_veiculo(id_album)
