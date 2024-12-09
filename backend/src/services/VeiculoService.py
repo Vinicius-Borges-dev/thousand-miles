b8346fac463ce024b754edc0bbe17c6397a26732
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-12-09 17:12:29 -0300

    Criação: Model para os veículos.

---
+++ backend/src/services/VeiculoService.py
@@ -0,0 +1,172 @@
+from src.models import (
+    VeiculoModel,
+    ModeloModel,
+    CambioModel,
+    CategoriaModel,
+    CombustivelModel,
+)
+from sqlalchemy.exc import SQLAlchemyError
+from flask import current_app as app
+from sqlalchemy.sql.expression import func
+
+class VeiculoService:
+
+    def criar_veículo(self, dados: dict):
+        try:
+            veiculo = VeiculoModel(
+                ano_fabricacao=dados.get("ano_fabricacao"),
+                assentos=dados.get("assentos"),
+                cor=dados.get("cor"),
+                disponivel=dados.get("disponivel"),
+                placa=dados.get("placa"),
+                preco_por_dia=dados.get("preco_por_dia"),
+                id_album_veiculo=dados.get("id_album_veiculo"),
+                id_categoria=dados.get("id_categoria"),
+                id_cambio=dados.get("id_cambio"),
+                id_combustivel=dados.get("id_combustivel"),
+                id_modelo=dados.get("id_modelo"),
+            )
+            app.session.add(veiculo)
+            app.session.commit()
+
+            return veiculo
+        except SQLAlchemyError as erro:
+            app.session.rollback()
+            raise erro
+
+    def buscar_veiculo_por_placa(self, placa: str):
+        try:
+            veiculo = app.session.query(VeiculoModel).filter_by(placa=placa).first()
+            return veiculo
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_veiculo_por_modelo(self, modelo: str):
+        try:
+            veiculo = (
+                app.session.query(VeiculoModel)
+                .join(ModeloModel)
+                .filter(ModeloModel.nome_modelo == modelo)
+                .first()
+            )
+
+            return veiculo
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_veiculo_por_id(self, id_veiculo: int):
+        try:
+            veiculo = (
+                app.session.query(VeiculoModel).filter_by(id_veiculo=id_veiculo).first()
+            )
+            return veiculo
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_todos_veiculos(self):
+        try:
+            veiculos = app.session.query(VeiculoModel).all()
+            return veiculos
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_veiculo_aleatorio_disponivel(self):
+        try:
+            veiculo = (
+                app.session.query(VeiculoModel)
+                .filter(VeiculoModel.disponivel == 1)
+                .order_by(func.random())
+                .first()
+            )
+            return veiculo
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def atualizar_veiculo(self, id_veiculo: int, dados: dict):
+        try:
+            veiculo = self.buscar_veiculo_por_id(id_veiculo)
+            veiculo.ano_fabricacao = dados.get("ano_fabricacao")
+            veiculo.assentos = dados.get("assentos")
+            veiculo.cor = dados.get("cor")
+            veiculo.disponivel = dados.get("disponivel")
+            veiculo.placa = dados.get("placa")
+            veiculo.preco_por_dia = dados.get("preco_por_dia")
+            veiculo.id_album_veiculo = dados.get("id_album_veiculo")
+            veiculo.id_categoria = dados.get("id_categoria")
+            veiculo.id_cambio = dados.get("id_cambio")
+            veiculo.id_combustivel = dados.get("id_combustivel")
+            veiculo.id_modelo = dados.get("id_modelo")
+
+            app.session.commit()
+
+            return veiculo
+        except SQLAlchemyError as erro:
+            app.session.rollback()
+            raise erro
+
+    def buscar_todos_veiculos_por_modelo(self, modelo: str):
+        try:
+            veiculos = (
+                app.session.query(VeiculoModel)
+                .join(ModeloModel)
+                .filter(ModeloModel.nome_modelo == modelo)
+                .all()
+            )
+            return veiculos
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_todos_veiculos_por_categoria(self, categoria: str):
+        try:
+            veiculos = (
+                app.session.query(VeiculoModel)
+                .join(CategoriaModel)
+                .filter(CategoriaModel.nome_categoria == categoria)
+                .all()
+            )
+            return veiculos
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_todos_veiculos_por_cambio(self, cambio: str):
+        try:
+            veiculos = (
+                app.session.query(VeiculoModel)
+                .join(CambioModel)
+                .filter(CambioModel.tipo_cambio == cambio)
+                .all()
+            )
+            return veiculos
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def buscar_todos_veiculos_por_combustivel(self, combustivel: str):
+        try:
+            veiculos = (
+                app.session.query(VeiculoModel)
+                .join(CombustivelModel)
+                .filter(CombustivelModel.tipo_combustivel == combustivel)
+                .all()
+            )
+            return veiculos
+        except SQLAlchemyError as erro:
+            raise erro
+
+    def alterar_disponibilidade_veiculo(self, id_veiculo: int, disponibilidade: int):
+        try:
+            veiculo = self.buscar_veiculo_por_id(id_veiculo)
+            veiculo.disponivel = disponibilidade
+            app.session.commit()
+            return veiculo
+        except SQLAlchemyError as erro:
+            app.session.rollback()
+            raise erro
+
+    def deletar_veiculo(self, id_veiculo: int):
+        try:
+            veiculo = self.buscar_veiculo_por_id(id_veiculo)
+            app.session.delete(veiculo)
+            app.session.commit()
+        except SQLAlchemyError as erro:
+            app.session.rollback()
+            raise erro
