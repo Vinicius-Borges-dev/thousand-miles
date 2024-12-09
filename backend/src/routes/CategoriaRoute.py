c55cb1274915606e82e9a2b69f3920232e2c253b
Vinicius-Borges-Dev <dev.vinicius.borges@outlook.com>
2024-12-09 17:15:46 -0300

    Criação: Rota para a categoria.

---
+++ backend/src/routes/CategoriaRoute.py
@@ -0,0 +1,29 @@
+from flask import Blueprint
+from src.controllers.CategoriaController import CategoriaController
+
+categoria_bp = Blueprint("categorias", __name__)
+
+
+@categoria_bp.route("/", methods=["POST"])
+def criar_categoria():
+    return CategoriaController().criar_categoria()
+
+@categoria_bp.route("/", methods=["GET"])
+def buscar_todas_categorias():
+    return CategoriaController().buscar_todas_categorias()
+
+@categoria_bp.route("/<int:id_categoria>", methods=["GET"])
+def buscar_categoria_por_id(id_categoria: int):
+    return CategoriaController().buscar_categoria_por_id(id_categoria)
+
+@categoria_bp.route("/nome/<string:nome_categoria>", methods=["GET"])
+def buscar_categoria_por_nome(nome_categoria: str):
+    return CategoriaController().buscar_categoria_por_nome(nome_categoria)
+
+@categoria_bp.route("/<int:id_categoria>", methods=["PUT"])
+def atualizar_categoria(id_categoria: int):
+    return CategoriaController().atualizar_categoria(id_categoria)
+
+@categoria_bp.route("/<int:id_categoria>", methods=["DELETE"])
+def deletar_categoria(id_categoria: int):
+    return CategoriaController().deletar_categoria(id_categoria)
